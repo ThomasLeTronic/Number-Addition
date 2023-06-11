c43a6ac224f7e9ca7c6a40424f636cbc77da6890
Tom <thomasburley803@gmail.com>
2023-06-11 09:35:48 +0100

    Update Program.cs
    seperated brackets

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-﻿namespace Qq{
-    class Qq{
-        static void Main(){
+﻿namespace Qq
+ {
+    class Qq
+    {
+        static void Main()
+        {
             Console.WriteLine("enter numbers then enter -1 to stop)");
             int int2=0;
             int int1=0;
-            while(int1!=-1){
+            while(int1!=-1)
+            {
                 int1=Convert.ToInt32(Console.ReadLine());
-                if(int1!=-1){
+                if(int1!=-1)
+                {
                     int2+=int1;
                 }
             }
